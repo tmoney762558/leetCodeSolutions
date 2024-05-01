afdba3e7e0489d5983df5b24fad2ec3a8614c824
Ty <131617288+tmoney762558@users.noreply.github.com>
2024-05-01 01:43:04 -0500

    Update leetCode2.cpp

--- leetCode2.cpp
+++ leetCode2.cpp
@@ -11,68 +11,44 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* current = l1;
-        ListNode* l3 = NULL;
-        int l1Val = 0;
-        int l2Val = 0;
-        long long mult = 1;
-        std::stack<int> stk;
+        ListNode *l3 = new ListNode;
+        ListNode *current = l3;
 
-        if (l1->val != 0) {
-            while (current != NULL) {
-                stk.push(current->val);
+        int carry = 0;
+
+        while (l1 != NULL || l2 != NULL) // We iterate until BOTH lists are empty
+        { 
+            int sum = 0 + carry; // Carry is carried over from previous iterations
+
+            if (l1 != NULL) // Continue going through list 1 until we reach NULL
+            {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != NULL) // Continue going through list 1 until we reach NULL
+            {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10; // Calculate the carry first
+            sum = sum % 10; //Calculating the single digit sum for our current position
+            
+            current->val = sum;
+
+            if (l1 != NULL || l2 != NULL)
+            {
+                current->next = new ListNode;
                 current = current->next;
             }
-
-            while (!stk.empty()) {
-                l1Val += stk.top() * mult;
-                stk.pop();
-                mult *= 10;
-            }
         }
-
-        mult = 1;
-        current = l2;
-        if (l2->val != 0) {
-            while (current != NULL) {
-                stk.push(current->val);
-                current = current->next;
-            }
-
-            while (!stk.empty()) {
-                l2Val += stk.top() * mult;
-                stk.pop();
-                mult *= 10;
-            }
-        }
-
-        int l3Val = l1Val + l2Val;
-
-        if (l3Val == 0) {
-            l3 = new ListNode;
-            l3->val = 0;
-            l3->next = NULL;
-            return l3;
-        }
-
-        while (l3Val != 0) {
-            if (l3 == NULL) {
-                l3 = new ListNode;
-                l3->val = l3Val % 10;
-                l3->next = NULL;
-                current = l3;
-            }
-
-            else {
-                ListNode* newNode = new ListNode;
-                newNode->val = l3Val % 10;
-                newNode->next = NULL;
-                current->next = newNode;
-                current = current->next;
-                current->next = NULL;
-            }
-
-            l3Val /= 10;
+        while (carry != 0)
+        {
+            current->next = new ListNode;
+            current = current->next;
+            current->val = carry % 10;
+            carry /= 10;
         }
         return l3;
     }
