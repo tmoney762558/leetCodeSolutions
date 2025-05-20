7eed601cabe64d4481fe9484f34e240a0984e2eb
Ty <131617288+tmoney762558@users.noreply.github.com>
2025-05-20 17:51:28 -0500

    Update binarySearch.ts (Adjusted to account for edge case)

--- binarySearch.ts
+++ binarySearch.ts
@@ -2,18 +2,18 @@ function search(nums: number[], target: number): number {
     let i = Math.floor(nums.length / 2);
     let range = i;
 
-    
-    while(nums[i] !== target) {
+    while (nums[i] !== target) {
         if (nums[i] < target) {
             range = Math.ceil(range / 2);
             i += range;
+
         }
         else if (nums[i] > target) {
             range = Math.ceil(range / 2);
             i -= range;
         }
         if (range <= 1 && nums[i] !== target) {
-            return -1;
+            return nums[i + 1] === target ? i + 1 : nums[i - 1] === target ? i - 1 : -1;
         }
     }
     return i;
