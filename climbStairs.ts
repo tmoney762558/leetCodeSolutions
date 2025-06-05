66bfcd6ee1ce5a732af6657d2c1d483e53dcbe0b
Ty <131617288+tmoney762558@users.noreply.github.com>
2025-06-05 18:01:11 -0500

    Update climbStairs.ts

--- climbStairs.ts
+++ climbStairs.ts
@@ -1,22 +1,13 @@
 function climbStairs(n: number): number {
-    const map = new Map();
-
-    function climb(s) {
-        if (s === 1) {
-            return 1;
+    const dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for (let i = 0; i < n + 1; i++) {
+        if (i > 2) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        if (s === 2) {
-            return 2;
-        }
-
-        if (map.has(s)) {
-            return map.get(s);
-        }
-
-        const result = climb(s - 1) + climb(s - 2);
-        map.set(s, result);
-        return result;
     }
 
-    return climb(n);
+    return dp[n];
 };
