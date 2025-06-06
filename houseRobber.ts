91890470a51a4bcf8094efaa277fe9cbbea431d5
Ty <131617288+tmoney762558@users.noreply.github.com>
2025-06-06 02:33:19 -0500

    Create houseRobber.ts

---
+++ houseRobber.ts
@@ -0,0 +1,15 @@
+function rob(nums: number[]): number {
+    if (nums.length === 1) {
+        return nums[0];
+    }
+
+    const dp = new Array(nums.length).fill(0);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+
+    for (let i = 2; i < nums.length; i++) {
+        dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+    }
+
+    return Math.max(dp[dp.length - 1], dp[dp.length - 2]);
+};
